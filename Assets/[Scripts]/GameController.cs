5374b328fa611a0dc16408b2976f301e7e583109
Juan <juan.dgb.99@gmail.com>
2021-09-28 17:29:01 -0400

    Controller

--- Assets/[Scripts]/GameController.cs
+++ Assets/[Scripts]/GameController.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+
+    public Rect screen;
+    public Rect safeArea;
+
+    public RectTransform backButtonRect;
+    public RectTransform nextButtonRect;
+
+    public Vector2 backButtonAnchor;
+    public Vector2 nextButtonAnchor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        backButtonAnchor = backButtonRect.anchoredPosition;
+        nextButtonAnchor = nextButtonRect.anchoredPosition;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        screen.width = Screen.width;
+        screen.height = Screen.height;
+        safeArea = Screen.safeArea;
+        float xOffset = safeArea.width - screen.width;
+
+        // Debug.Log( Screen.orientation);
+
+        switch (Screen.orientation)
+        {
+
+            case ScreenOrientation.LandscapeLeft:
+                backButtonRect.anchoredPosition = new Vector2(backButtonAnchor.x + safeArea.x, backButtonAnchor.y);
+                nextButtonRect.anchoredPosition = new Vector2(nextButtonAnchor.x + xOffset, nextButtonAnchor.y);
+                break;
+            case ScreenOrientation.LandscapeRight:
+                backButtonRect.anchoredPosition = new Vector2(backButtonAnchor.x + safeArea.x, backButtonAnchor.y);
+                nextButtonRect.anchoredPosition = new Vector2(nextButtonAnchor.x + xOffset, nextButtonAnchor.y);
+                break;
+            case ScreenOrientation.Portrait:
+                backButtonRect.anchoredPosition = new Vector2(backButtonAnchor.x, backButtonAnchor.y);
+                nextButtonRect.anchoredPosition = new Vector2(nextButtonAnchor.x, nextButtonAnchor.y);
+                break;
+            default:
+                break;
+        }
+
     }
 }
